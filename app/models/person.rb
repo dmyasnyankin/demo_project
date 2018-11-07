4fdae10be8e45f2d8d8ef0b33093f47723a5d00f
Dennis <dmyasnyankin@berkeley.edu>
2018-11-07 00:36:56 -0800

    Work with setting up associations on rails

---
+++ app/models/person.rb
@@ -0,0 +1,9 @@
+class Person < ActiveRecord::database
+  validates :name, presence: true
+  validates :house, presence: true
+
+ belongs_to :house,
+  primary_key: :id,
+  foreign_key: :house_id,
+  class: 'House'
+end
